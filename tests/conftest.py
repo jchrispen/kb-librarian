9f5b4758f0f90012ea440dfad38e8679e9b4b0ed
Jason <jason.chrispen@gmail.com>
2026-06-27 20:06:11 -0400

    Color pytest progress percentages

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,23 @@
 from types import MethodType
 
+import pytest
+
+
+PROGRESS_COLORS = {
+    "failed": "red",
+    "error": "red",
+    "passed": "green",
+    "skipped": "yellow",
+}
+
+
+@pytest.hookimpl(hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    outcome = yield
+    report = outcome.get_result()
+    if report.when == "call" or report.outcome in {"failed", "skipped"}:
+        item.config._kb_last_progress_outcome = report.outcome
+
 
 def pytest_sessionstart(session):
     terminal_reporter = session.config.pluginmanager.get_plugin("terminalreporter")
@@ -10,7 +28,9 @@ def pytest_sessionstart(session):
         msg = self._get_progress_information_message()
         width = self._width_of_current_line
         fill = self._tw.fullwidth - width - 1
-        self.write(msg.rjust(fill), flush=True)
+        outcome = getattr(self.config, "_kb_last_progress_outcome", "passed")
+        color = PROGRESS_COLORS.get(outcome, "yellow")
+        self.write(msg.rjust(fill), flush=True, **{color: True})
 
     terminal_reporter._write_progress_information_filling_space = MethodType(
         write_progress_information_filling_space,
